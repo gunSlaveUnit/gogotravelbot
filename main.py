2c4baab0cdac8bd8cae263812afa5e66a8971735
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-08-21 16:41:16 +0300

    Trying to draw a main menu screen

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 from dotenv import dotenv_values
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher, executor, types
+
+
+async def main_menu_screen(message: types.Message):
+	# TODO: normal database query for user balance
+	dummy_balance = 236
+
+	buttons = [
+		[
+			types.KeyboardButton("Find driver"),
+			types.KeyboardButton("Find passenger"),
+		],
+		[
+			types.KeyboardButton(f"Balance - {dummy_balance} Go"),
+			types.KeyboardButton("History of trips"),
+		],
+		[
+			types.KeyboardButton("Profile"),
+		],
+		[
+			types.KeyboardButton("FAQ"),
+			types.KeyboardButton("Support"),
+		],
+	]
+
+	keyboard_markup = types.ReplyKeyboardMarkup(
+		keyboard=buttons,
+		resize_keyboard=True,
+		input_field_placeholder="Select a menu item"
+	)
+
+	# TODO: normal database query for user name
+	dummy_username = "Poul"
+
+	await message.answer(text=f"{dummy_username}, have a nice trip!", reply_markup=keyboard_markup)
+
 
 if __name__ == "__main__":
 	config = dotenv_values(".env")
 
 	bot = Bot(config['API_TOKEN'])
 	dispatcher = Dispatcher(bot)
+
+	dispatcher.register_message_handler(main_menu_screen, commands=["start"])
 
 	executor.start_polling(dispatcher, skip_updates=True)
