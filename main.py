0c447f51039cbe71c32f2d6fa5537ef3f93bb3e0
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-08-21 16:08:58 +0300

    aiogram bot creation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from dotenv import dotenv_values
+from aiogram import Bot, Dispatcher, executor
 
 if __name__ == "__main__":
 	config = dotenv_values(".env")
+
+	bot = Bot(config['API_TOKEN'])
+	dispatcher = Dispatcher(bot)
