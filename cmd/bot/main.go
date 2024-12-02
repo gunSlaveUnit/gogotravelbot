51d51406acdb570927c4e786221c7c9b51a6af8d
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-12-02 22:17:15 +0300

    Main menu. Select role logic

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -9,6 +9,32 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("1"),
+		tgbotapi.NewKeyboardButton("2"),
+		tgbotapi.NewKeyboardButton("3"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("4"),
+		tgbotapi.NewKeyboardButton("5"),
+		tgbotapi.NewKeyboardButton("6"),
+	),
+)
+
+var startMenu = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("trip"),
+		tgbotapi.NewKeyboardButton("help"),
+	),
+)
+var roleMenu = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("driver"),
+		tgbotapi.NewKeyboardButton("passenger"),
+	),
+)
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -21,6 +47,8 @@ func main() {
 	}
 	token := os.Getenv("TOKEN")
 
+	states := make(map[string]string)
+
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		fmt.Println(err)
@@ -31,7 +59,46 @@ func main() {
 	updateConfig.Timeout = 30
 
 	for update := range bot.GetUpdatesChan(updateConfig) {
+		username := update.Message.From.UserName
+		fmt.Println(username)
+
+		state, exists := states[username]
+		fmt.Println(state)
+
+		if !exists {
+			states[username] = "start"
+		}
+
 		message := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		text := update.Message.Text
+
+		switch states[username] {
+		case "start":
+			switch text {
+			case "trip":
+				states[username] = "trip"
+				message.Text = "Are you a driver or a passenger?"
+				message.ReplyMarkup = roleMenu
+			default:
+				message.Text = "What do you want to do?"
+				message.ReplyMarkup = startMenu
+			}
+		case "trip":
+			states[username] = "role"
+
+			switch text {
+			case "driver":
+				message.Text = "How many people do you want to take?"
+				message.ReplyMarkup = numericKeyboard
+			case "passenger":
+				message.Text = "How many people will go?"
+				message.ReplyMarkup = numericKeyboard
+			}
+		case "role":
+			message.Text = fmt.Sprintf("%s: %s", states[username], text)
+			states[username] = "start" // just for now
+		}
+
 		if _, err := bot.Send(message); err != nil {
 			fmt.Println(err)
 		}
